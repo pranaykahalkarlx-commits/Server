5846a5c1775c918b84f616f1ef60e5973d6306ca
Pranay LonarX <pranaykahalkar.lx@gmail.com>
2026-03-21 20:38:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -67,5 +67,5 @@ app.post('/api/auth/verify-otp', (req, res) => {
 
 app.get('/', (req, res) => res.send('AI Receptionist Backend Running'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
